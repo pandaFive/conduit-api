a3d8d3023a124f6ac3a55dd09528bf3e0cdfadcc
propan <ldspdk5v@gmail.com>
2023-12-31 02:48:17 +0900

    Finish authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  include JsonWebToken
+
+  before_action :authenticated?
+
+  def authenticated?
+    token = request.headers["Authorization"]
+    token = token.chomp.split(" ").last if header
+
+    begin
+      @decoded = JsonWebToken.decode(token)
+      @current_user = User.find(@decoded["user_id"])
+    rescue ActiveRecord::RecordNotFound
+      render_unuthorized
+    rescue JWT::DecodeError
+      render_unuthorized
+    end
+  end
+
+  def render_unuthorized
+    render json: { error: "unuthorized", status: 401 }
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+  include JsonWebToken
+  skip_before_action :authenticated?, only: [:registration]
+
   def registration
+    user = User.new(user_params)
+
+    if user.save
+      render json: create_render_json(user)
+    else
+      render json: { message: user.errors.full_messages.join(" "), status: 422 }, status: :unprocessable_entity
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+    def create_render_json(user)
+      token = JsonWebToken.encode({ user_id: user[:id] })
+      response = { user: { email: user[:email], token:, username: user[:username], bio: user[:bio], image: nil } }
+      response
+    end
 end
